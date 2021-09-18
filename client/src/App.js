92c977e7ead3bdd2fdbad8fb48524cb4f22f19b9
travis martin <travismartin266@gmail.com>
2021-09-18 20:33:33 -0300

    adding some more features

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
     return (
         <div className="container">
             <h1>Create Post</h1>
             <PostCreate />
+            <hr />
+            <h1>Post Lists</h1>
+            <PostList />
         </div>
     );
 };
