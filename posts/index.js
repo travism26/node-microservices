2f216b7dc21344154198a9f7d58a4858502e29a3
travismartin266 <travismartin266@gmail.com>
2021-09-18 10:54:42 -0300

    fixed endpoint issue missing brackets after json

--- posts/index.js
+++ posts/index.js
@@ -2,32 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
-const { response } = require('express');
 
 const app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
-app.get('/posts', function (req, res) {
-    // res.send(posts);
-    res.send('Hello world');
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.post('/posts', function (req, res) {
-    res.send('Hello world');
-    // const id = randomBytes(4).toString('hex');
-    // const { title } = req.body;
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-    // posts[id] = {
-    //     id,
-    //     title
-    // };
+  posts[id] = {
+    id,
+    title
+  };
 
-    // res.status(201).send('Passed');
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
-    console.log('listening on 4000');
+  console.log('Listening on 4000');
 });
